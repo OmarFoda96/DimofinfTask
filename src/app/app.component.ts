c46cd7ea1c4673425d6cd5a2123e7e24d951f430
OmarFoda96 <OmarFoda96@gmail.com>
2021-01-10 23:13:40 +0200

    todo list is done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'DimofinfTask';
+  editMode: boolean = false;
+  tasks: any[] = [];
+  form: FormGroup = new FormGroup({
+    task: new FormControl(''),
+  });
+  selectedItem;
+  selectedIndex;
+  openEditMode(item, index) {
+    this.selectedIndex = index;
+    this.selectedItem = item;
+    this.editMode=true
+    this.form.controls.task.setValue(item.name)
+  }
+  deleteItem(item, index) {
+    this.tasks.splice(index,1)
+  }
+  addTask(e) {
+    if (this.editMode) {
+      this.tasks.splice(this.selectedIndex,1,{
+        name: e.target.value,
+        id: this.tasks.length + 1,
+        selected:false
+      })
+      this.form.reset();
+      this.editMode=false
+    } else {
+      this.tasks.push({
+        name: e.target.value,
+        id: this.tasks.length + 1,
+        selected:false
+      });
+      this.form.reset();
+      this.editMode=false
+    }
+  }
+  selectedMode(item){
+    item.selected=true
+    this.tasks.sort((a,b)=>{return a.selected-b.selected})
+  }
 }
